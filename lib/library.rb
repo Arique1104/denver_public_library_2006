75e60946cee5b6b016821f05bce33bccb451986e
Arique Aguilar <arique1104@gmail.com>
2020-07-22 14:44:15 -0500

    Publication time frame

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,13 @@ class Library
     @authors << author
   end
 
+  def publication_time_frame_for(author)
+    # { |a,b| a.length <=> b.length }
+    sorted_books = author.books.sort { |a,b| a.publication_year.to_i  <=> b.publication_year.to_i}
+
+    {
+      start: sorted_books.first.publication_year,
+    end: sorted_books.last.publication_year}
+  end
+
 end
