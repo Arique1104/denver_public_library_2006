4c36f1b10577eed3cf6d6e166d164a92be81840d
Arique Aguilar <arique1104@gmail.com>
2020-07-22 13:35:25 -0500

    Creates Test is exists

--- test/book_test.rb
+++ test/book_test.rb
@@ -5,9 +5,11 @@ require "./lib/book"
 
 class BookTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    
+    assert_instance_of Book, book
+  end
 
   #def test_it_has_attributes
   #end
