25a7b960a153daf6246cb3c9ffc45d8407db1cba
Arique Aguilar <arique1104@gmail.com>
2020-07-22 13:43:44 -0500

    Creates test it exists for author test

--- test/author_test.rb
+++ test/author_test.rb
@@ -6,9 +6,11 @@ require "./lib/author"
 
 class AuthorTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_instance_of Author, charlotte_bronte
+  end
 
   #def test_it_has_attributes
   #end
