35a478157a14b9aaa5eb9c745d06f4678b9de78d
Arique Aguilar <arique1104@gmail.com>
2020-07-22 14:18:03 -0500

    Adds required files to LIbarary test

---
+++ test/library_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/book"
+require "./lib/author"
+require "./lib/library"
+
+class LibraryTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
