7b502f9c7ed90384407c97faa7537cc21adc18f2
Arique Aguilar <arique1104@gmail.com>
2020-07-22 13:39:13 -0500

    Creates author method for book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,9 @@
 class Book
- attr_reader  :title
+ attr_reader  :title,
+              :author
   def initialize(data)
     @title = data[:title]
+    @author = data[:author_first_name] + " " + data[:author_last_name]
 
   end
 
