58afea477a94062d39f74635222b5cd304e6f42e
Arique Aguilar <arique1104@gmail.com>
2020-07-22 14:02:05 -0500

    Creates write method that instantiates a Book instance within the Author class

--- lib/author.rb
+++ lib/author.rb
@@ -4,7 +4,17 @@ class Author
   def initialize(name)
     @name = name[:first_name] + " " + name[:last_name]
     @books = []
-
   end
+
+  def write(title, publication_year)
+    author_array = @name.gsub(/\s+/m, ' ').strip.split(" ")
+      Book.new({
+      author_first_name: author_array[0],
+      author_last_name: author_array[1],
+      publication_date: publication_year,
+      title: title
+      })
+  end
+
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -20,4 +20,35 @@ class AuthorTest < Minitest::Test
     assert_equal [], charlotte_bronte.books
   end
 
+  def test_it_can_create_book_objects
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_equal Book, jane_eyre.class
+
+    assert_equal "Jane Eyre", jane_eyre.title
+
+
+
+  end
+
+  def test_it_can_add_books_to_array
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal [], charlotte_bronte.books
+
+
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    charlotte_bronte.write("Villette", "1853")
+
+
+
+    assert_equal 2, charlotte_bronte.books.count
+    assert_equal Array, charlotte_bronte.books.class
+
+    #=> [#<Book:0x00007fb896e22538...>, #<Book:0x00007fb8980aaca0...>]
+  end
+
 end
