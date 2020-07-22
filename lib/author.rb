2a0c13b360dd0e9d4f87bb37db46e8fee89d4c55
Arique Aguilar <arique1104@gmail.com>
2020-07-22 14:17:08 -0500

    Creates library class files

--- lib/author.rb
+++ lib/author.rb
@@ -8,12 +8,14 @@ class Author
 
   def write(title, publication_year)
     author_array = @name.gsub(/\s+/m, ' ').strip.split(" ")
-      Book.new({
+
+      @books << Book.new({
       author_first_name: author_array[0],
       author_last_name: author_array[1],
       publication_date: publication_year,
       title: title
       })
+
   end
 
 
--- test/author_test.rb
+++ test/author_test.rb
@@ -23,13 +23,8 @@ class AuthorTest < Minitest::Test
   def test_it_can_create_book_objects
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
-    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-
-    assert_equal Book, jane_eyre.class
-
-    assert_equal "Jane Eyre", jane_eyre.title
-
-
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    
 
   end
 
@@ -43,12 +38,9 @@ class AuthorTest < Minitest::Test
 
     charlotte_bronte.write("Villette", "1853")
 
-
-
     assert_equal 2, charlotte_bronte.books.count
     assert_equal Array, charlotte_bronte.books.class
 
-    #=> [#<Book:0x00007fb896e22538...>, #<Book:0x00007fb8980aaca0...>]
   end
 
 end
