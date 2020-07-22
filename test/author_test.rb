b3f17d724db73412588e2070f0a2218f6fce1f50
Arique Aguilar <arique1104@gmail.com>
2020-07-22 13:42:54 -0500

    Sets required files for author test

---
+++ test/author_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/book"
+require "./lib/author"
+
+class AuthorTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
